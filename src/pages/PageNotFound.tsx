e185f94d071c32ba957ade282c781e75b12aac7a
alexprimak58 <aprimak43@gmail.com>
2025-03-25 00:07:25 +0200

    removed unused imports

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { useMoveBack } from '../hooks/useMoveBack';
 
 function PageNotFound() {
--- src/pages/Questions.tsx
+++ src/pages/Questions.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import Question from '../ui/Question';
